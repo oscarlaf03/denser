09be32d6a2b01013f2cdad988326060bf926001b
oscar ortiz <ortizg.oscar@gmail.com>
2018-05-04 20:53:10 -0300

    First version done

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,5 @@
 class Photo < ApplicationRecord
   belongs_to :product
   mount_uploader :image, PhotoUploader
+  validates :product, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+if User.find_by(email: 'ortizg.oscar@gmail.com').nil?
+  oscar = User.new(email: 'ortizg.oscar@gmail.com')
+  oscar.manager = true
+  oscar.save
+  else
+  oscar = User.find_by(email: 'ortizg.oscar@gmail.com')
+  oscar.manager = true
+  oscar.save
+end
+
+if User.find_by(email: 'jose.ortiz@denser.com.mx').nil?
+  jose = User.new(email: 'jose.ortiz@denser.com.mx')
+  jose.manager = true
+  jose.save
+  else
+  jose = User.find_by(email: 'jose.ortiz@denser.com.mx')
+  jose.manager = true
+  jose.save
+end
+
+vaso = Product.new
+vaso.name = 'Vaso deportivo'
+vaso.description = 'Con este práctico y elegante vaso siempre estarás hidratado y listo para cualquier aventura'
+vaso.category = 'cocina'
+vaso.item_type = 'vaso'
+vaso[:banner] = "image/upload/v1525393783/yepns2aa2hfjqutezecl.png"
+vaso[:main_photo] = "image/upload/v1525393780/upgw7tjfhqi3hnrmmvvh.png"
+vaso.measures = '20 x 25'
+vaso.colores = %w[ verde amarillo rojo blanco ]
+vaso.capacity = 0.5
+vaso.save
